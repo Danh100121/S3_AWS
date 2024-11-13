2796c230b906a43fd88add4b36ffa1236ea0792f
PHAM TIEN DANH(Pham Tien Danh - Steve) <danh100121@lgcns.com>
2024-11-13 16:47:40 +0700

    upload file using s3 aws

---
+++ src/main/java/com/danhpahm/s3storagesfiles/S3StoragesFilesApplication.java
@@ -0,0 +1,13 @@
+package com.danhpahm.s3storagesfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3StoragesFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3StoragesFilesApplication.class, args);
+    }
+
+}
